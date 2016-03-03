abdd212a9ad2add31e2940bfff4ae409b40a9903
Bernadette Bandayrel <bandayrelb@gmail.com>
2016-03-02 21:45:22 -0800

    added animation to bow when firing added arrow when drawing arrow falls after being instantiated bow doesn't return to default position after firing

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -4,18 +4,20 @@ using System.Collections;
 public class PlayerAttack : MonoBehaviour {
 
     Animation drawBow;
-    //Animator bow;
+    Animator idleBow;
     private bool isDraw = false;
+    public Rigidbody arrow;
+    public Transform bow;
+    Rigidbody clone;
 
-	// Use this for initialization
 	void Start () {
         drawBow = GetComponent<Animation>();
-        //bow = GetComponent<Animator>();
+        idleBow = GetComponent<Animator>();
         drawBow["Draw"].layer = 1;
         drawBow["Draw"].wrapMode = WrapMode.Once;
+        drawBow["pullBowClip"].wrapMode = WrapMode.Once;
 	}
 	
-	// Update is called once per frame
 	void Update () {
 
         if(Input.GetButton("Fire1") == false)
@@ -27,15 +29,17 @@ public class PlayerAttack : MonoBehaviour {
 
 	    if(Input.GetButton("Fire1") && isDraw == false)
         {
-            //bow.SetTrigger( "pull" );
+            clone = Instantiate( arrow, transform.position, transform.rotation ) as Rigidbody;
+            drawBow.Play( "pullBowClip" );
             drawBow.Play("Draw");
             isDraw = true;
         }
 
         else if(!Input.GetButton("Fire1") && isDraw == true)
         {
-            //bow.SetTrigger( "release" );
             drawBow.Play("IdleBow");
+            clone.AddForce( transform.forward * 100 );
+            idleBow.SetTrigger( "stopFire" );
             isDraw = false;
         }
 	}
