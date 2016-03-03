a955f20b5a461093751450b16275cb41125ec72f
Bernadette Bandayrel <bandayrelb@gmail.com>
2016-03-02 18:49:15 -0800

    created git proj

---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAttack : MonoBehaviour {
+
+    Animation drawBow;
+    //Animator bow;
+    private bool isDraw = false;
+
+	// Use this for initialization
+	void Start () {
+        drawBow = GetComponent<Animation>();
+        //bow = GetComponent<Animator>();
+        drawBow["Draw"].layer = 1;
+        drawBow["Draw"].wrapMode = WrapMode.Once;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(Input.GetButton("Fire1") == false)
+        {
+            drawBow.Stop();
+            drawBow.Play("IdleBow");
+            isDraw = false;
+        }
+
+	    if(Input.GetButton("Fire1") && isDraw == false)
+        {
+            //bow.SetTrigger( "pull" );
+            drawBow.Play("Draw");
+            isDraw = true;
+        }
+
+        else if(!Input.GetButton("Fire1") && isDraw == true)
+        {
+            //bow.SetTrigger( "release" );
+            drawBow.Play("IdleBow");
+            isDraw = false;
+        }
+	}
+}
